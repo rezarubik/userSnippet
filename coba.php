1193f3b8cf7a666f610628efff583906aa6c51f5
Muhammad Reza Pahlevi Y <muhammad.reza.pahlevi.y@gmail.com>
2020-01-08 14:02:09 +0700

    Init Commit

---
+++ coba.php
@@ -0,0 +1,13 @@
+<label for="name">Username</label>
+<input type="text" name="name" id="name">
+
+<label for="password">Username</label>
+<input type="password" name="password" id="password">
+
+<label for="password">Password</label>
+<input type="password" name="password" id="password">
+
+<label>
+    Username
+    <input type="text">
+</label>
